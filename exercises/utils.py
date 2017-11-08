5019c05be9931dac736f458a2e9e11fa86d6f741
Shwetank Kumar <shwetank@hoodline.com>
2017-11-08 09:32:14 -0800

    Added a function to plot based on a list of files

--- exercises/utils.py
+++ exercises/utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+from PIL import Image
 from matplotlib import pyplot as plt
 
 def plot_batch(batch, labels, classes=[0,1]):
@@ -18,3 +19,18 @@ def plot_batch(batch, labels, classes=[0,1]):
                 plt.imshow(img)
                 sp[img_num_batch].set_title(classes[int(labels[img_num_batch])],
                 fontsize=12)
+
+def plot_file_list(img_file_list):
+    f = plt.figure(figsize=(18,6))
+    sp=[]
+    n_c = 8
+    n_r = len(img_file_list)//n_c+1
+    for n in range(1,n_r+1):
+        for m in range(1,n_c+1):
+            img_num_plot = n_c*(n-1) + m
+            img_num_batch = img_num_plot - 1
+            if len(img_file_list)>=img_num_plot:
+                sp.append(f.add_subplot(n_r,n_c,img_num_plot))
+                sp[img_num_batch].axis('Off')
+                img = Image.open(img_file_list[img_num_batch])
+                plt.imshow(img)
