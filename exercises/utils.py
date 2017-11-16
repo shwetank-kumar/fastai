03323a69e761bc946f9e0a73b2c34d2c7ee7e992
Shwetank Kumar <shwetank@hoodline.com>
2017-11-15 19:44:00 -0800

    Vgg16 with Batchnormalization

--- exercises/utils.py
+++ exercises/utils.py
@@ -1,8 +1,11 @@
 import numpy as np
 from PIL import Image
 from matplotlib import pyplot as plt
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten, Input
+from keras.layers import Conv2D, MaxPooling2D, BatchNormalization
 
-def plot_batch(batch, labels, classes=[0,1]):
+def plot_batch(batch, labels, classes):
     # batch = batch.astype(np.uint8)
     f = plt.figure(figsize=(12,6))
     sp = []
@@ -17,7 +20,7 @@ def plot_batch(batch, labels, classes=[0,1]):
                 sp[img_num_batch].axis('Off')
                 img = batch[img_num_batch].astype(np.uint8)
                 plt.imshow(img)
-                sp[img_num_batch].set_title(classes[int(labels[img_num_batch])],
+                sp[img_num_batch].set_title(classes[np.squeeze(np.nonzero(labels[img_num_batch]))],
                 fontsize=12)
 
 def plot_file_list(img_file_list):
@@ -34,3 +37,57 @@ def plot_file_list(img_file_list):
                 sp[img_num_batch].axis('Off')
                 img = Image.open(img_file_list[img_num_batch])
                 plt.imshow(img)
+
+class Vgg16BN():
+    def __init__(self, size=(224,224,3), include_top=True):
+        self.create(size,include_top)
+
+    def create(self,size,include_top):
+        if size!=(224,224,3):
+            include_top=False
+
+        model=self.model=Sequential()
+        model.add(BatchNormalization(input_shape=size))
+        model.add(Conv2D(64, (3,3), activation='relu', padding='same'))
+        model.add(Conv2D(64, (3,3), activation='relu', padding='same',
+         input_shape=size))
+        model.add(MaxPooling2D())
+        model.add(BatchNormalization())
+        model.add(Conv2D(128, (3,3), activation='relu', padding='same',
+         input_shape=size))
+        model.add(Conv2D(128, (3,3), activation='relu', padding='same',
+         input_shape=size))
+        model.add(MaxPooling2D())
+        model.add(BatchNormalization())
+        model.add(Conv2D(256, (3,3), activation='relu', padding='same',
+         input_shape=size))
+        model.add(Conv2D(256, (3,3), activation='relu', padding='same',
+         input_shape=size))
+        model.add(Conv2D(256, (3,3), activation='relu', padding='same',
+         input_shape=size))
+        model.add(MaxPooling2D())
+        model.add(BatchNormalization())
+        model.add(Conv2D(512, (3,3), activation='relu', padding='same',
+         input_shape=size))
+        model.add(Conv2D(512, (3,3), activation='relu', padding='same',
+         input_shape=size))
+        model.add(Conv2D(512, (3,3), activation='relu', padding='same',
+         input_shape=size))
+        model.add(MaxPooling2D())
+        model.add(BatchNormalization())
+        model.add(Conv2D(512, (3,3), activation='relu', padding='same',
+         input_shape=size))
+        model.add(Conv2D(512, (3,3), activation='relu', padding='same',
+         input_shape=size))
+        model.add(Conv2D(512, (3,3), activation='relu', padding='same',
+         input_shape=size))
+        model.add(MaxPooling2D())
+        model.add(Flatten())
+        model.add(BatchNormalization())
+        model.add(Dense(4096, activation='relu'))
+        model.add(BatchNormalization())
+        model.add(Dropout(0.5))
+        model.add(Dense(4096, activation='relu'))
+        model.add(BatchNormalization())
+        model.add(Dropout(0.5))
+        model.add(Dense(10, activation='softmax'))
